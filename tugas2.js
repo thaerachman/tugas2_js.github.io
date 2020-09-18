6c5b204f12db7a74c0067123446ab63af31b530f
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-09-18 11:26:28 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var nama = "Torich";
+var nim = 123;
+var stat_kwn = true;
+var no_hp = null;
+var hobi;
+var teman = ["hasan","budi","vega"];
+
+console.log (nama, nim, stat_kwn, no_hp, hobi, teman)
